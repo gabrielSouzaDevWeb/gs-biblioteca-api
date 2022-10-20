dc916fdc5601e852878071fe5c4336ea117e0cff
Gabriel Souza <gabriel.souza@publicsoft.com.br>
2022-10-20 14:38:58 -0300

    dockerfile

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node 
+
+WORKDIR /usr/src/app
+
+COPY package*.json .
+
+RUN npm i
+
+COPY . .
+
+RUN npm run build
+
+EXPOSE 3000
+
+CMD ["node", "dist/main.js"]
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Req } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
+import { Res } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
   constructor() {}
+  @Get('/')
+  HandlerHello(@Res() res, @Req() req) {
+    return res.status(HttpStatus.OK).json({ message: 'Hello World!' });
+  }
 }
