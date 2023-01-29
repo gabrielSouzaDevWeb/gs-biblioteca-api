9358b8a9d417528ffea29ac7f9d6db6cacc86a3e
Gabriel Souza <gabrielsouzadevweb@gmail.com>
2023-01-29 15:33:31 -0300

    feat: auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { AuthController } from './auth.controller';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '6000s' },
+      signOptions: { expiresIn: '600s' },
       // signOptions: { expiresIn: '6000s', algorithm: 'RS256' },
     }),
   ],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -22,7 +22,7 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { email: user.email, senha: user.senha };
+    const payload = { nome: user.nome, nivelAcesso: user.nivelAcesso };
     return {
       access_token: this.jwtService.sign(payload),
     };
