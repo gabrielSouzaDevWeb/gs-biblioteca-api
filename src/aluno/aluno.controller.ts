5b67e225e4c816e4a07236ba508c8f6aa63b5e70
Gabriel Souza <gabrielsouzadevweb@gmail.com>
2023-04-21 08:24:51 -0300

    feat: detalhe da tabela mockado

--- src/aluno/aluno.controller.ts
+++ src/aluno/aluno.controller.ts
@@ -70,6 +70,24 @@ export class AlunoController {
       });
   }
 
+  @Get('detalhe')
+  async pegarDetalhe(@Res() res, @Req() req) {
+    return res.status(HttpStatus.OK).json({
+      message: 'Consulta realizada com sucesso!',
+      data: {
+        idPrivado: 1,
+        idPublico: '1',
+        nomLivro: '12 Regras para a vida',
+        nomAutor: 'Jordan B. Peterson',
+        categoria: 'Auto ajuda',
+        estante: 'suspense',
+        prateleira: '3',
+        paginas: 7,
+        unidades: 1,
+      },
+    });
+  }
+
   @Put('atualizar/:idPrivado')
   async atualizarAluno(
     @Res() res,
